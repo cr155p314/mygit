3dc620a235de2b4aaf55bf8d8f53b8e6e183d490
cr155p314 <christian.putschky@web.de>
2020-01-16 21:05:52 +0100

    chang3e

--- javascript/index.js
+++ javascript/index.js
@@ -1,5 +1,11 @@
 
-let name = 'Mosh';
+let name = 'Pupi';
 console.log(name);
+
 // Cannot be a reserved keyword
 // Should be meaningful
+//cannot start with a number(1name)
+//cannot contain a space or hyphen (-)
+// are case-sensitive
+
+let firstName;
