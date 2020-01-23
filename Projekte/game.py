f35a8c2661774ef726ab17b73b6ec9e27eb18532
cr155p314 <christian.putschky@web.de>
2020-01-23 21:47:36 +0100

    makeup

--- Projekte/game.py
+++ Projekte/game.py
@@ -1,7 +1,9 @@
 from random import randint
 
+
 game_running = True
 game_results = []
+
 
 def calculate_monster_attack(attack_min, attack_max):
     return randint(attack_min, attack_max)
@@ -10,14 +12,15 @@ def calculate_monster_attack(attack_min, attack_max):
 def game_ends(winner_name):
     print(f'{winner_name} won the game')
 
+
 while game_running == True:
     counter = 0
     new_round = True
-    player = {'name': 'Chriss', 'attack': 13, 'heal': 16, 'health': 100}
-    monster = {'name': 'Bubu', 'attack_min': 10, 'attack_max': 20,  'health': 100}
-   
+    player = {'name': 'Chriss', 'attack': 13,
+              'heal': 16, 'health': 100}
+    monster = {'name': 'Bubu', 'attack_min': 10,
+               'attack_max': 20,  'health': 100}
 
-    
     print('Enter your Name')
     player['name'] = input()
     print('---' * 7)
@@ -51,24 +54,21 @@ while game_running == True:
 
         elif player_choice == '2':
             player['health'] = player['health'] + player['heal']
-            
+
             player['health'] = player['health'] - calculate_monster_attack(monster['attack_min'], monster['attack_max'])
             if player['health'] <= 0:
                 monster_won = True
-
 
         elif player_choice == '3':
             new_round = False
             game_running = False
 
-
         elif player_choice == '4':
-            for stat in game_result:
+            for stat in game_results:
                 print(stat)
 
         else:
             print('Invalid Input')
- 
 
         if player_won == False and monster_won == False:
             print(player['name'] + ' has ' + str(player['health']) + ' left')
@@ -76,19 +76,19 @@ while game_running == True:
 
         elif player_won:
             game_ends(player['name'])
-            round_result = {'name': player['name'], 'health': player['health'], 'rounds': counter}
+            round_result = {'name': player['name'],
+                            'health': player['health'], 'rounds': counter}
             game_results.append(round_result)
             print(game_results)
             new_round = False
-        
 
         elif monster_won:
             game_ends(monster['name'])
-            round_result = {'name': player['name'], 'health': player['health'], 'rounds': counter}
+            round_result = {'name': player['name'],
+                            'health': player['health'], 'rounds': counter}
             game_results.append(round_result)
             print(game_results)
-            new_round = False      
+            new_round = False
 
         if player_won == True or monster_won == True:
             new_round = False
-
